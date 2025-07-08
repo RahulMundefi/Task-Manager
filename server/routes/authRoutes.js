71f6ffae0a88ff91caedea6c6ec89f723a982d27
Rahul Mundefi <mundefirahul@gmail.com>
2025-07-08 12:50:47 +0530

    server running

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,10 @@
+// routes/authRoutes.js
+import express from 'express';
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Auth route is working!");
+});
+
+export default router;
